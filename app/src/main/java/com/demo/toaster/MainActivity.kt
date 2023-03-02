d9deb1356ad863ebaea57c4138815cace71ada15
Nikunj India <contact@yamiitsolutions.com>
2023-03-02 18:02:19 +0530

    Initial commit

---
+++ app/src/main/java/com/demo/toaster/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.demo.toaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+}
